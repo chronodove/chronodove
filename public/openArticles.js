ae43fb2866ce2592578a12ed03e7f43385594b1e
chronodove <63011633+chronodove@users.noreply.github.com>
2025-01-20 07:41:04 -0600

    writing page up w/tutorial

--- public/openArticles.js
+++ public/openArticles.js
@@ -2,31 +2,30 @@
 const amount = document.getElementsByTagName('article');
 let host = window.location.href;
 
-
 function showThis(pass){
-  //console.log(pass);
-
+  //console.log(pass); //for testing purposes
   //check all articles to find targeted one
   for (let i = 0; i < amount.length; i++){
-    //console.log("Check: ", amount[i].id);
+    //console.log("Check: ", amount[i].id); //for testing purposes
     let target = document.getElementById(pass);
     //see if that needs to be shown or not
     if (amount[i].id === target.id){
-      //console.log("actions: show!");
+      //console.log("actions: show!"); //for testing purposes
       amount[i].style.display = 'block';
       amount[i].style.overflow = 'scroll';
 
       //check if this is the diary page 
       if (host.includes("diary.html")){
+        //write a message if there was no entry
         let ps = amount[i].getElementsByTagName('p');
         if (ps.length === 0){
-          console.log("this is running");
+          //console.log("this is running"); //for testing purposes
           amount[i].innerHTML = "<p>looks like nothing was written. i wonder what happened.</p>"
         }
       }
 
     } else {
-      //console.log("action: hide!");
+      //console.log("action: hide!"); //for testing purposes
       amount[i].style.display = 'none';
     }
   }
@@ -36,14 +35,5 @@ function showThis(pass){
 function reset() {
   for (let i = 0; i < amount.length; i++) {
     amount[i].style.display = 'block';
-  }
-}
-
-
-
-//continuously check for empty articles
-for(let i = 0; i < amount.length; i++){
-  if (amount[i].getElementsByTagName('p')){
-
   }
 }
