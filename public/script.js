a3c80ac7112906510c8d3db94009d3147c13f2ba
chronodove <63011633+chronodove@users.noreply.github.com>
2025-01-26 17:00:33 -0600

    little changes + entries + game dev update

--- public/script.js
+++ public/script.js
@@ -1,3 +1,16 @@
+//date function
+/*window.addEventListener('DOMContentLoaded', () => {
+   const d = new Date();
+   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+   let day = d.getDay() + 1;
+   let month = months[d.getMonth()];
+
+   
+
+});*/
+
+
 //navigation
 function goBack(){
    document.write('<span id="link" onclick="history.back()">go back</span>');
