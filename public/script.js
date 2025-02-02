b7cf206b1b079ca0cab6ceaf31baa40e5b805431
chronodove <63011633+chronodove@users.noreply.github.com>
2025-02-01 19:08:52 -0600

    february

--- public/script.js
+++ public/script.js
@@ -1,17 +1,60 @@
-//date function
-/*window.addEventListener('DOMContentLoaded', () => {
-   const d = new Date();
-   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-   let day = d.getDay() + 1;
-   let month = months[d.getMonth()];
-
-   
-
-});*/
-
-
 //navigation
 function goBack(){
    document.write('<span id="link" onclick="history.back()">go back</span>');
+}
+
+//loading spinner w/notes explaining what each part of the code does
+//followed this tutorial (with some edits) -> https://youtu.be/q76TexbMXJg?si=Gp5w0oSYyFKgooFB
+
+//see if the webpage has finished loading all resources or not. if so, preform the following function 
+window.addEventListener("load", () => {
+   //define loader
+   const LOADER = document.querySelector(".loader");
+   //if it has finished loading, initialize the "loader-hidden" class; hiding the loader but continuing the animation
+   LOADER.classList.add("loader-hidden");
+   //checks to see if the transition is over and once it does...
+   LOADER.addEventListener("transitionend", () => {
+   //...completely remove loader element from html, stopping the website from continuing to play the animation 
+   document.body.removeChild(LOADER);
+   })
+})
+
+//define amount of articles
+const amount = document.getElementsByTagName('article');
+let host = window.location.href;
+
+function showThis(pass){
+  //console.log(pass); //for testing purposes
+  //check all articles to find targeted one
+  for (let i = 0; i < amount.length; i++){
+    //console.log("Check: ", amount[i].id); //for testing purposes
+    let target = document.getElementById(pass);
+    //see if that needs to be shown or not
+    if (amount[i].id === target.id){
+      //console.log("actions: show!"); //for testing purposes
+      amount[i].style.display = 'block';
+      amount[i].style.overflow = 'scroll';
+
+      //check if this is the diary page 
+      if (host.includes("diary.html")){
+        //write a message if there was no entry
+        let ps = amount[i].getElementsByTagName('p');
+        if (ps.length === 0){
+          //console.log("this is running"); //for testing purposes
+          amount[i].innerHTML = "<p>looks like nothing's written. what a shame, maybe some day.</p>"
+        }
+      }
+
+    } else {
+      //console.log("action: hide!"); //for testing purposes
+      amount[i].style.display = 'none';
+    }
+  }
+}
+
+//show all entries
+function reset() {
+  for (let i = 0; i < amount.length; i++) {
+    amount[i].style.display = 'block';
+  }
 }
